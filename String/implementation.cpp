720e6f354577347bc79e927367594c85c7ec2f9a
Hrach <86060764+Hrach-Harutyunyan@users.noreply.github.com>
2022-02-25 00:40:08 +0400

    Add files via upload

---
+++ String/implementation.cpp
@@ -0,0 +1,122 @@
+#include "myString.h"
+
+MyString::MyString () : _m_buffer {new char[1]} , _m_size {} {
+    _m_buffer[0] = '\0';
+}
+
+MyString::MyString (const char* rhs) : _m_buffer {new char[1]},_m_size {}  {
+    if (rhs) {
+        for (int i{}; rhs[i] != '\0'; ++i) {
+            this->push_back (rhs[i]);
+        }
+    }
+}
+
+MyString::MyString (const MyString& rhs) : _m_buffer {} , _m_size {} {
+    if (rhs[0] == '\0'){
+        _m_buffer = new char[1];
+        _m_buffer[0] = '\0';
+        _m_size = 0;
+    } else {
+        _m_buffer = new char[rhs._m_size + 1];
+        for (int i = 0; rhs[i] != '\0'; i++) {
+            this->push_back (rhs[i]);
+        }
+    }
+} 
+
+MyString::MyString (MyString&& rhs) : _m_buffer {} , _m_size {} {
+    this->_m_buffer = rhs._m_buffer;
+    rhs._m_buffer = nullptr;
+    this ->_m_size = rhs._m_size;
+    rhs._m_size = 0;
+}
+
+MyString& MyString::operator = (const MyString& rhs) {
+    if(this == &rhs) return *this;
+    _m_buffer[0] = '\0';
+    this->_m_size = 0;
+    for (int i = 0; rhs[i] != '\0'; i++) {
+        this->push_back (rhs[i]);
+    }
+    return *this;
+}
+
+MyString& MyString::operator = (MyString&& rhs) {
+    this->_m_buffer = rhs._m_buffer;
+    rhs._m_buffer = nullptr;
+    this ->_m_size = rhs._m_size;
+    rhs._m_size = 0;
+    return *this;
+};
+
+const char MyString::operator[] (int index) const {
+    return this->_m_buffer[index];
+};
+
+bool MyString::operator == (const MyString& other) const {
+    bool result = true;
+    if (this->_m_size != other._m_size) return !result;
+    for (int i{}; (*this)[i] != '\0'; ++i) {
+        if ((*this)[i] != other[i])
+        return !result;
+    }
+    return result;
+};
+
+void MyString::push_back (const char element) {
+    this->_m_buffer[_m_size] = element;
+    this->_m_buffer[++_m_size] = '\0';
+};
+
+char MyString::pop_back () {
+    if (_m_size) {
+        char ch = this->_m_buffer[--_m_size];
+        this->_m_buffer[_m_size] = '\0';
+        return ch;
+    }
+}
+
+bool MyString::insert (const int index, const char element) {
+    bool result = true;
+    if(index >= 0 && index <= _m_size) {
+        char* temp = new char[++_m_size];
+        for(int indexTemp{}, indexBuffer{}; indexTemp <= _m_size && indexBuffer < _m_size ;) {
+            if(indexTemp == index) {
+                temp[indexTemp++] = element;
+            } else {
+                temp[indexTemp++] = _m_buffer[indexBuffer++];
+            }
+        }
+        delete[] _m_buffer;
+        _m_buffer = temp;
+        temp = nullptr;
+        return result;
+    }
+    return !result;
+};
+
+
+int MyString::find (const char element) {
+    for (int i = 0; (*this)[i] != '\0'; i++) {
+        if((*this)[i] == element) return i;
+    }
+    return -1;
+}
+
+char MyString::at (const int index) {
+    try {
+        if(index >= 0 && index < _m_size)
+        return (*this)[index];
+        throw (std::exception());
+    }
+    catch(const std::exception& e) {
+        std::cerr << e.what () << ": Wrong index:\n";
+    }
+    
+}
+
+MyString::~MyString () {
+    delete[] _m_buffer;
+    _m_buffer = nullptr;
+};
---
+++ String/myString.h
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class MyString
+{
+public:
+    MyString ();
+    explicit MyString (const char*);
+    MyString (const MyString&);
+    MyString (MyString&&);
+    ~MyString ();
+    MyString& operator = (const MyString&);
+    MyString& operator = (MyString&&);
+    friend const MyString operator + (const MyString& lhs,const MyString& rhs) {
+        MyString temp;
+        for (int i{}; lhs[i] != '\0'; i++) {
+            temp.push_back(lhs[i]);
+        }
+        for (int j{}; rhs[j] != '\0'; ++j) {
+            temp.push_back(rhs[j]);
+        }
+        return temp;  
+    };
+    friend std::ostream& operator << (std::ostream& print, const MyString& obj) {
+        print << obj._m_buffer;
+        return print;
+    };
+    const char operator[] (int) const;
+    bool operator == (const MyString&) const;
+    const int get_length() const {return _m_size;};
+    void push_back (const char);
+    char pop_back ();
+    bool insert (const int, const char);
+    char at(const int);
+    int find(const char);
+
+private:
+    char* _m_buffer {};
+    int _m_size {};
+};
